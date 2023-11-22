f9579e36bb0ad8748c14cdd4452ec3b9c6c1c605
miguel22211 <miguel.ibannezg@estudiante.uam.es>
2023-11-22 18:39:28 +0100

    minor changes

--- front/front_api/src/app/app-routing.module.ts
+++ front/front_api/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { FormularioFiltrosComponent } from './formulario-filtros/formulario-filtros.component';
 import { EstadisticasComponent } from './estadisticas/estadisticas.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'formulario-filtros', component: FormularioFiltrosComponent },
   { path: 'estadisticas', component: EstadisticasComponent },
 ];
 
--- front/front_api/src/app/app.module.ts
+++ front/front_api/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApiService } from './api.service';
 import { HomeComponent } from './home/home.component';
-import { FormularioFiltrosComponent } from './formulario-filtros/formulario-filtros.component';
 import { FormsModule } from '@angular/forms';
 import { EstadisticasComponent } from './estadisticas/estadisticas.component';
 import { SeleccionService } from './seleccion.service';
@@ -16,7 +15,6 @@ import { TagCloudComponent } from 'angular-tag-cloud-module';
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioFiltrosComponent,
     HomeComponent,
     EstadisticasComponent
   
